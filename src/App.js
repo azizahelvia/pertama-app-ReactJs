443a91ca82575afe19fb82604beaf8633909633c
azizahelvia <azizahelviaa@gmail.com>
2020-11-04 16:12:04 +0700

    Aplikasi pertama ReactJs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let namaSiswa = 'Azizah Elvia Shofiyah Salsabila';
+  let absenSiswa = 2;
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Nama: {namaSiswa}</h1>
+        <h2>Absen: {absenSiswa}</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
